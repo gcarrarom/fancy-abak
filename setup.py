d9775a1e76951aeb3b16888f9e72c577ed42e07c
Gui Martins <gmartins@fancywhale.ca>
2021-04-29 19:33:36 -0300

    Wrong package name

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='abak',
-    version='0.0.1',
+    name='fancy-abak',
+    version='0.0.2',
     author='Gui Martins',
     url='https://fancywhale.ca/',
     author_email='gmartins@fancywhale.ca',
     packages=find_packages(),
     include_package_data=True,
-    py_modules=['abak'],
+    py_modules=['fancy-abak'],
     install_requires=[
-        'Click',
-        'requests'
+        'click',
+        'requests',
+        'tabulate'
     ],
     entry_points='''
         [console_scripts]
