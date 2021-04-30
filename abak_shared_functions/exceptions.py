809e8b9f337189f09a38a9c35c2902762345d03c
Gui Martins <gmartins@fancywhale.ca>
2021-04-30 14:15:35 -0300

    Better exception format

--- abak_shared_functions/exceptions.py
+++ abak_shared_functions/exceptions.py
@@ -2,4 +2,4 @@ from click import BadParameter
 
 class Sorry(BadParameter):
     def __init__(self, message):
-        super().__init__('Oh hey there! Sorry bud, but ' + message + '!')
+        super().__init__('Oh, hey there! Sorry bud, but ' + message + '!')
