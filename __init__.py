c513dcaff246a34aeb8aa477365c01e19e858888
Gui Martins <gmartins@fancywhale.ca>
2021-05-06 15:39:39 -0300

    Silly mistake! Sorry :D

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from timesheets import timesheets
-from client import clients
-from project import projects
-from abak_config import config
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='fancy-abak',
-    version='0.0.4',
+    version='0.0.5',
     author='Gui Martins',
     url='https://fancywhale.ca/',
     author_email='gmartins@fancywhale.ca',
     packages=find_packages(),
     include_package_data=True,
-    py_modules=['fancy-abak'],
+    py_modules=['abak'],
     install_requires=[
         'click',
         'requests',
